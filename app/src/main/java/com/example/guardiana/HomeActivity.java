516fd278e524fe1d424a6e06d89dd2bd974e753b
JoniKarta <jkarta26@gmail.com>
2020-12-25 17:44:37 +0200

    added search fragment

---
+++ app/src/main/java/com/example/guardiana/HomeActivity.java
@@ -0,0 +1,35 @@
+package com.example.guardiana;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.guardiana.fragments.FragmentRoad;
+import com.example.guardiana.fragments.FragmentSearch;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+public class HomeActivity extends AppCompatActivity {
+    private ChipNavigationBar chipNavigationBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        chipNavigationBar = findViewById(R.id.bottom_nav_menu);
+        chipNavigationBar.setItemSelected(R.id.bottom_nav_search, true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentSearch()).commit();
+        bottomMenu();
+    }
+    private void bottomMenu() {
+        chipNavigationBar.setOnItemSelectedListener(id -> {
+            Fragment fragment = null;
+            if (R.id.bottom_nav_search == id) {
+                fragment = new FragmentSearch();
+            } else if(R.id.bottom_nav_map == id){
+                fragment = new FragmentRoad();
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+        });
+    }
+}
