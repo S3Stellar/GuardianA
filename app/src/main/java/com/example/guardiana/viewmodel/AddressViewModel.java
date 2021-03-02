4d36e5fda01e83aca36f8e10413292ca321a7ae9
JoniKarta <Jkarta26@gmail.com>
2021-03-02 22:24:19 +0200

    added delete operation

--- app/src/main/java/com/example/guardiana/viewmodel/AddressViewModel.java
+++ app/src/main/java/com/example/guardiana/viewmodel/AddressViewModel.java
@@ -7,6 +7,10 @@ import com.example.guardiana.model.Address;
 import com.example.guardiana.repository.AddressRepository;
 import com.example.guardiana.repository.AddressResponse;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class AddressViewModel extends ViewModel {
 
     private final AddressRepository addressRepository;
@@ -22,6 +26,10 @@ public class AddressViewModel extends ViewModel {
 
     public LiveData<AddressResponse> getAllAddresses(String userId, String type, String value, String sortBy, String sortOrder, int page, int size, int offset) {
         return addressRepository.getAllAddresses(userId, type, value, sortBy, sortOrder, page, size, offset);
+    }
+
+    public LiveData<AddressResponse> delete(String addressId, int position){
+        return addressRepository.delete(addressId, position);
     }
 
 }
